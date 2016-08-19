ac52475f9ee41076b01020653578f90632038e75
shangguan <shangguan@pku.edu.cn>
2016-08-19 18:42:17 +0800

    generate mock sed data

--- mock_data_sed.py
+++ mock_data_sed.py
@@ -1,48 +1,123 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cPickle as pickle
-from gaussian_model import MultiGaussian, GaussianModelDiscrete
+from collections import OrderedDict
+from fitter import bandfunc as bf
+from fitter import basicclass as bc
+from fitter.sed import sedclass as sedsc
+from fitter.sed.model_functions import Modified_BlackBody
+ls_mic = 2.99792458e14 #micron/s
 
 #Generate the mock data#
 #-----------------#
-Ndata  = 50
-xMax   = 800.0
-Nmodel = 10
+## Creat an SedClass object
+targname = 'mock'
+redshift = 0.2
+bandList = ['w1', 'w2', 'w3', 'w4', 'PACS_70', 'PACS_100', 'PACS_160', 'SPIRE_250', 'SPIRE_350', 'SPIRE_500']
+nBands = len(bandList)
+sedwave = np.array([3.353, 4.603, 11.561, 22.088, 70.0, 100.0, 160.0, 250.0, 350.0, 500.0])
+fakedata = np.ones(nBands)
+phtData = {'WISE&Herschel': bc.DiscreteSet(bandList, sedwave, fakedata, fakedata, fakedata)}
+sedData = sedsc.SedClass(targname, redshift, phtDict=phtData)
+DL = sedData.dl
+
+## Load the bandpass
+### Load WISE bandpass
+wisePath = '/Users/jinyi/Work/PG_QSO/filters/wise/'
+wiseBandDict = OrderedDict()
+bandCenterList = [3.353, 4.603, 11.561, 22.088] #Isophotal wavelength
+for n in range(4):
+    bandName = 'w{0}'.format(n+1)
+    bandFile = '{0}.dat'.format(bandName)
+    bandPck = np.genfromtxt(wisePath+bandFile)
+    bandWave = bandPck[:, 0]
+    bandRsr = bandPck[:, 1]
+    bandCenter = bandCenterList[n]
+    wiseBandDict[bandName] = sedsc.BandPass(bandWave, bandRsr, bandCenter, bandName=bandName)
+sedData.add_bandpass(wiseBandDict)
+### Load Herschel bandpass
+fp = open('/Users/jinyi/Work/PG_QSO/filters/herschel/herschel_bandpass.dict', 'r')
+herschelBands = pickle.load(fp)
+fp.close()
+herschelBandList = herschelBands.keys()
+herschelBandList.remove('README')
+#print herschelBandList
+herschelBandDict = OrderedDict()
+for bandName in herschelBandList:
+    #Be careful with the sequence!
+    bandWave = ls_mic / herschelBands[bandName]['bandpass'][0][::-1]
+    bandRsr = herschelBands[bandName]['bandpass'][1][::-1]
+    bandCenter = herschelBands[bandName]['wave0']
+    herschelBandDict[bandName] = sedsc.BandPass(bandWave, bandRsr, bandCenter,
+                                             bandFunc=bf.BandFunc_Herschel,
+                                             bandName=bandName)
+sedData.add_bandpass(herschelBandDict)
+
+## Generate the mock data
+
 fAdd   = None #0.1
-pRange = [
-    [5.0, 20.0],   #The range of a
-    [20.0, 580.0], #The range of b
-    [50.0, 200.0], #The range of c
-]
+logMList = [1.2, 4.0, 9.4]
+TList = [641.8, 147.4, 26.1]
+betaList = [1.7, 2.3, 2.0]
 print( "#------Start fitting------#" )
-print( "# Ndata: {0}".format(Ndata) )
-print( "# Nmodel: {0}".format(Nmodel) )
+parList = []
+for loop in range(3):
+    pTuple = (logMList[loop], TList[loop], betaList[loop])
+    parList.append(pTuple)
+    print("MBB{0}:logM ({1[0]}),  T ({1[1]}), beta ({1[2]})".format(loop, pTuple))
 print( "# f_add: {0}".format(fAdd) )
 print( "#-------------------------#" )
 
-xData = np.linspace(1.0, xMax, Ndata)
-model = MultiGaussian(xData, pRange, Nmodel, fAdd)
-yTrue = model['y_true']
-yObsr = model['y_obsr']
-yErr = model['y_err']
-pValue = model['parameters']
-rangeList = model['ranges']
-cmpList = model['compnents']
-model['x'] = xData
+Ndata  = 500
+xMax   = 500.0
+x = 10**np.linspace(0.0, np.log10(xMax), Ndata)
+yTrue = np.zeros(Ndata)
+cmpList = []
+for loop in range(3):
+    logM, T, beta = parList[loop]
+    yCmp = Modified_BlackBody(logM, T, beta, x, DL)
+    yTrue += np.array(yCmp)
+    cmpList.append(yCmp)
+yTrueBand = sedData.model_pht(x, yTrue)
+yTrueBand = np.array(yTrueBand)
+#yErr = 5.0 + 30.0 * np.random.rand(nBands)
+yErr = np.concatenate([1.0 * np.ones(4), 10.0*np.ones(3), 20.0*np.ones(3)])
+yObsr = yTrueBand.copy()
+if not fAdd is None:
+    print("The model uncertainty is considered!")
+    yObsr += np.abs(fAdd * yObsr) * np.random.rand(nBands)
+yObsr += yErr * np.random.randn(nBands)
+model = {}
+model['x']          = x
+model['x_obsr']     = sedwave
+model['y_obsr']     = yObsr
+model['y_err']      = yErr
+model['parameters'] = parList
+model['compnents']  = cmpList
 
-for p in pValue:
-    print("a: {0[0]}, b: {0[1]}, c:{0[2]}".format(p))
-
-fileName = "gauss{0}.dict".format(Nmodel)
+fileName = "mbb_mock.dict"
 fp = open(fileName, "w")
 pickle.dump(model, fp)
 fp.close()
 print("{0} is saved!".format(fileName))
+sedArray = np.array([sedwave, yObsr, yErr, np.ones(nBands)])
+sedArray = np.transpose(sedArray)
+sedFileName = "mock.sed"
+fp = open(sedFileName, "w")
+fp.write("wavelength\tflux\tsigma\n")
+np.savetxt(fp, sedArray, fmt='%.3f', delimiter='\t')
+fp.close()
 
 fig = plt.figure()
-plt.errorbar(xData, yObsr, yerr=yErr, fmt=".k")
-plt.plot(xData, yTrue, linewidth=1.5, color="k")
+plt.errorbar(sedwave, yObsr, yerr=yErr, fmt=".r")
+plt.scatter(sedwave, yTrueBand, linewidth=1.5, color="k")
 for y in cmpList:
-    plt.plot(xData, y, linestyle='--')
-plt.savefig("gauss{0}.pdf".format(Nmodel))
+    plt.plot(x, y, linestyle='--')
+plt.plot(x, yTrue, color="k")
+ymax = np.max(yTrue)
+plt.xlim([1.0, 6e2])
+plt.ylim([1e1, ymax*2.0])
+plt.xscale("log")
+plt.yscale("log")
+plt.savefig("mbb_mock.pdf")
 plt.close()
