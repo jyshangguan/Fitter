ed512cb4f66e02890e575af95d6cdfc8705ab385
shangguan <shangguan@pku.edu.cn>
2016-09-11 20:43:36 +0800

    Generate mock data with lnf

--- mock/mock_data_dl07.py
+++ mock/mock_data_dl07.py
@@ -7,15 +7,135 @@ from sedfit.fitter import basicclass as bc
 from sedfit import sedclass        as sedsc
 from sedfit import model_functions as sedmf
 from sedfit import fit_functions   as sedff
+
 ls_mic = 2.99792458e14 #micron/s
-inputModelDict = sedmf.inputModelDict
 funcLib = sedmf.funcLib
-Model2Data = sedmf.Model2Data
+inputModelDict = OrderedDict(
+    (
+        ("Hot_Dust", {
+                "function": "Modified_BlackBody",
+                "logM": {
+                    "value": 0.32,
+                    "range": [-10.0, 3.0], #[0.3, 0.4], #
+                    "type": "c",
+                    "vary": False, #True,
+                    "latex": r"$\mathrm{log}\, M_\mathrm{d}$",
+                },
+                "beta": {
+                    "value": 2.0,
+                    "range": [1.5, 2.5], #[1.9, 2.1], #
+                    "type": "c",
+                    "vary": False, #True, #
+                    "latex": r"$\beta$",
+                },
+                "T": {
+                    "value": 846.77,
+                    "range": [500, 1300], #[846., 847], #
+                    "type": "c",
+                    "vary": False, #True,
+                    "latex": r"$T$",
+                }
+            }
+        ),
+        ("CLUMPY", {
+                "function": "CLUMPY_intp",
+                "logL": {
+                    "value": 46.4,
+                    "range": [40.0, 50.0], #[6.3, 6.4],
+                    "type": "c",
+                    "vary": True, #False, #
+                    "latex": r"$\mathrm{log}\,SF_\mathrm{Torus}$",
+                },
+                "i": {
+                    "value": 47.60,
+                    "range": [0.0, 90.0], #[47.0, 48.0], #
+                    "type": "c",
+                    "vary": True, #False, #
+                    "latex": r"$i$",
+                },
+                "tv": {
+                    "value": 17.53,
+                    "range": [10.0, 300.0], #[17, 18], #
+                    "type": "c",
+                    "vary": True, #False, #
+                    "latex": r"$\tau_\nu$",
+                },
+                "q": {
+                    "value": 0.7,
+                    "range": [0.0, 3.0], #[0.6, 0.8], #
+                    "type": "c",
+                    "vary": True, #False, #
+                    "latex": r"$q$",
+                },
+                "N0": {
+                    "value": 6.43,
+                    "range": [1.0, 15.0], #[6.42, 6.44], #
+                    "type": "c",
+                    "vary": True, #False, #
+                    "latex": r"$N_0$",
+                },
+                "sigma": {
+                    "value": 58.14,
+                    "range": [15.0, 70.0], #[58.0, 59.0], #
+                    "type": "c",
+                    "vary": True, #False, #
+                    "latex": r"$\sigma$",
+                },
+                "Y": {
+                    "value": 30.0,
+                    "range": [5.0, 100.0], #[29., 31.], #
+                    "type": "c",
+                    "vary": True, #False, #
+                    "latex": r"$Y$",
+                }
+            }
+        ),
+        ("DL07", {
+                "function": "DL07",
+                "umin": {
+                    "value": 10.0,
+                    "range": [0.1, 25.0],
+                    "type": "c",
+                    "vary": False, #True, #
+                    "latex": r"$U_\mathrm{min}$",
+                },
+                "umax": {
+                    "value": 1e6,
+                    "range": [1e3, 1e6],
+                    "type": "c",
+                    "vary": False, #True, #
+                    "latex": r"$U_\mathrm{max}$",
+                },
+                "qpah": {
+                    "value": 3.19,
+                    "range": [0.1, 4.58],
+                    "type": "c",
+                    "vary": False, #True, #
+                    "latex": r"$q_\mathrm{PAH}$",
+                },
+                "gamma": {
+                    "value": 0.02,
+                    "range": [0.01, 0.99], #[0.01, 0.03],
+                    "type": "c",
+                    "vary": False, #True, #
+                    "latex": r"$\gamma$",
+                },
+                "logMd": {
+                    "value": 9.12,
+                    "range": [8.0, 10.0], #[9.0, 10.0],
+                    "type": "c",
+                    "vary": False, #True, #
+                    "latex": r"$\mathrm{log}\,M_\mathrm{d}$",
+                }
+            }
+        ),
+    )
+)
 
 #Generate the mock data#
 #-----------------#
 ## Creat an SedClass object
-targname = "mock_dl07"
+targname = "mock_dl07_lnf"
 redshift = 0.2
 bandList = ["w1", "w2", "w3", "w4", "PACS_70", "PACS_100", "PACS_160", "SPIRE_250", "SPIRE_350", "SPIRE_500"]
 sedwave = np.array([3.353, 4.603, 11.561, 22.088, 70.0, 100.0, 160.0, 250.0, 350.0, 500.0])/(1+redshift)
@@ -69,12 +189,11 @@ sedData.add_bandpass(herschelBandDict)
 parAddDict_all = {
     "DL": DL,
 }
-fAdd   = None #0.02 #
-Ndata  = 700
-xMax   = 600.0
+fAdd   = 0.02 #None #
+Ndata  = 1000
+xMax   = 1000.
 waveModel = 10**np.linspace(0.0, np.log10(xMax), Ndata)
-sedModel = bc.Model_Generator(inputModelDict, funcLib, waveModel, parAddDict_all,
-                              model2data=Model2Data)
+sedModel = bc.Model_Generator(inputModelDict, funcLib, waveModel, parAddDict_all)
 parList = sedModel.get_parList()
 #print inputModelDict
 parNumber  = len(parList)
