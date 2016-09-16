3592f0295ad43dbde635efbda4650cdbe8292a11
shangguan <shangguan@pku.edu.cn>
2016-09-16 22:31:02 +0800

    modify fit_functions.py

--- sedfit/fit_functions.py
+++ sedfit/fit_functions.py
@@ -1,4 +1,6 @@
 import numpy as np
+import george
+from george import kernels
 
 def ChiSq(data, model, unct=None):
     '''
@@ -72,6 +74,38 @@ def Model2Data(sedModel, sedData):
     fluxModel = fluxModelPht + fluxModelSpc
     return fluxModel
 
+#Model to data function using Gaussian process regression
+def Model2Data_gp(sedModel, sedData):
+    """
+    Convert the continual model to the data-like model to directly
+    compare with the data.
+
+    Parameters
+    ----------
+    sedModel : ModelCombiner object
+        The combined model.
+    sedData : SEDClass object
+        The data set of SED.
+
+    Returns
+    -------
+    fluxModel : list
+        The model flux list of the data.
+
+    Notes
+    -----
+    None.
+    """
+    waveModel = sedModel.get_xList()
+    fluxModel = sedModel.combineResult()
+    fluxModelPht = sedData.model_pht(waveModel, fluxModel)
+    fluxModelSpc = sedData.model_spc(sedModel.combineResult)
+    fluxDict = {
+        "pht": fluxModelPht,
+        "spc": fluxModelSpc
+        }
+    return fluxDict
+
 #The log_likelihood function: for SED fitting
 def logLFunc(params, data, model):
     """
@@ -99,7 +133,6 @@ def logLFunc(params, data, model):
     nParVary = len(model.get_parVaryList())
     y = np.array(data.get_List('y'))
     e = np.array(data.get_List('e'))
-    #ym = np.array(model.combineResult(x))
     ym = np.array(Model2Data(model, data))
     if len(params) == nParVary:
         s = e
@@ -111,3 +144,52 @@ def logLFunc(params, data, model):
     #Calculate the log_likelihood
     logL = -0.5 * (ChiSq(y, ym, s) + np.sum( np.log(2 * np.pi * s**2) ))
     return logL
+
+#The log_likelihood function: for SED fitting using Gaussian process regression
+def logLFunc_gp(params, data, model):
+    """
+    Calculate the likelihood of data according to the model and its parameters.
+
+    Parameters
+    ----------
+    params : list
+        The variable parameter list of the model.
+    data : DataSet
+        The data need to fit.
+    model : ModelCombiner
+        The model to fit the data.
+
+    Returns
+    -------
+    lnL : float
+        The ln likelihood.
+
+    Notes
+    -----
+    None.
+    """
+    #Get the data and error
+    xSpc = np.array(data.get_csList("x"))
+    yPht = np.array(data.get_dsList("y"))
+    ySpc = np.array(data.get_csList("y"))
+    ePht = np.array(data.get_dsList("e"))
+    eSpc = np.array(data.get_csList("e"))
+    #Calculate the model
+    model.updateParList(params)
+    yDict = Model2Data_gp(model, data)
+    yPhtModel = np.array(yDict["pht"])
+    ySpcModel = np.array(yDict["spc"])
+    #Additional systematic uncertainties
+    nParVary = len(model.get_parVaryList())
+    f = np.exp(params[nParVary]) #The last par is lnf.
+    sPht = (ePht**2 + (yPhtModel * f)**2)**0.5
+    sSpc = (eSpc**2 + (ySpcModel * f)**2)**0.5
+    #lnlikelihood for spectral data using Gaussian process regression
+    a, tau = np.exp(params[nParVary:])
+    gp = george.GP(a * kernels.Matern32Kernel(tau))
+    gp.compute(xSpc, sSpc)
+    lnlSpc = gp.lnlikelihood(ySpc - ySpcModel)
+    #lnlikelihood for photometric data
+    lnlPht = -0.5 * (ChiSq(yPht, yPhtModel, sPht) + np.sum( np.log(2 * np.pi * sPht**2) ))
+    lnL = lnlPht + lnlSpc
+    return lnL
