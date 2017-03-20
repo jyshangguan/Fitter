5cf870966322e351412e5663d1d5bd58be22ab86
shangguan <shangguan@pku.edu.cn>
2017-03-20 09:09:55 +0800

    make the yticks appear

--- sedfit/mcmc/mcmc_emcee.py
+++ sedfit/mcmc/mcmc_emcee.py
@@ -615,7 +615,7 @@ class EmceeModel(object):
         #->Set the yaxis tick range
         yTickRange = ax.yaxis.get_majorticklocs()
         yTickRange = yTickRange[(yTickRange >= ymin) & (yTickRange <= ymax)]
-        ax.yaxis.set_ticks(yTickRange[1:-1])
+        #ax.yaxis.set_ticks(yTickRange[1:-1])
         ax.xaxis.set_tick_params(which="major", labelsize=18)
         ax.yaxis.set_tick_params(which="major", labelsize=18)
         ax.legend(loc="lower right", framealpha=0.3, fontsize=16)
