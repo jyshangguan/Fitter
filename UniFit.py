f9c01efe70e7cf9282cfdd3f07fb01691d8a14b4
shangguan <shangguan@pku.edu.cn>
2017-04-20 10:42:15 +0800

    use assigned config

--- UniFit.py
+++ UniFit.py
@@ -1,9 +1,5 @@
 from __future__ import print_function
 import os
-import sys
-import warnings
-import traceback
-import numpy as np
 from optparse import OptionParser
 from astropy.table import Table
 
@@ -19,26 +15,34 @@ def makeCommand(cDict):
     cLine = " ".join(commandList)
     return cLine
 
-#Parse the commands#
-#-------------------#
+#->Parse the commands
 parser = OptionParser()
+parser.add_option("-m", "--mpi_ncore", dest="ncores", default="1",
+                  help="Run the code with MPI using the asigned number of cores if ncores>1.")
+parser.add_option("-w", "--warning", dest="warning", action="store_true", default=False,
+                  help="Stop ignoring the warnings.")
+parser.add_option("-o", "--overwrite", dest="overwrite", action="store_true", default=False,
+                  help="Overwrite the object information with the command-line inputs.")
 parser.add_option("-l", "--list", dest="list", default=None,
                   help="Provide a list of target info to fit.")
-parser.add_option("-m", "--mpi_ncore", dest="ncores", default="1",
-                  help="Run the code with MPI using the ")
-parser.add_option("-r", "--refit", dest="refit",
-                  action="store_true", default=False,
-                  help="Refit the SED though there is a result found.")
-parser.add_option("-o", "--overwrite", dest="overwrite",
-                  action="store_true", default=False,
-                  help="Overwrite the object information with the command-line inputs.")
-parser.add_option("-w", "--warning", dest="warning",
-                  action="store_true", default=False,
-                  help="Stop ignoring the warnings.")
+parser.add_option("-f", "--list_format", dest="list_format", default="ascii.ipac",
+                  help="Provide a list of target info to fit.")
+parser.add_option("-c", "--config", dest="config", default=None,
+                  help="Assign the config file for a list of objects.")
 parser.add_option("-p", "--path", dest="path", default=None,
-                  help="The path of the SED data.")
+                  help="Assign the path of the SED data for a list of objects.")
+parser.add_option("-r", "--refit", dest="refit", action="store_true", default=False,
+                  help="Refit the SED though there is a result found. Only works with -l assigned.")
 (options, args) = parser.parse_args()
+if options.list is None:
+    if not options.config is None:
+        parser.error("Option -c only works with -l.")
+    if not options.path is None:
+        parser.error("Option -p only works with -l.")
+    if not options.refit is None:
+        parser.error("Option -r only works with -l.")
 
+#->Determine whether use MPI or not.
 commandDict = {}
 ncores = eval(options.ncores)
 if ncores == 1:
@@ -48,6 +52,8 @@ elif ncores > 1:
 commandDict = {
     "head": commandHead,
 }
+
+#->Determine whether to run a list of objects.
 targetList = options.list
 if targetList is None:
     commandDict["options"] = []
@@ -63,7 +69,7 @@ else: #If the target list is provided, fit the targets one by one.
     sedPath = options.path
     if sedPath is None:
         sedPath = ""
-    targTable = Table.read(targetList, format="ascii.ipac")
+    targTable = Table.read(targetList, format=options.list_format)
     nameList  = targTable["Name"]
     zList     = targTable["z"]
     sedList   = targTable["sed"]
@@ -71,7 +77,14 @@ else: #If the target list is provided, fit the targets one by one.
     for loop in range(len(nameList)):
         targname = nameList[loop]
         redshift = zList[loop]
-        commandArgs = [args[0], targname, "{0}".format(redshift)]
+        if options.config is None:
+            try:
+                configName = targTable["config"][loop]
+            except:
+                raise RuntimeError("Fail to find the config file information.")
+        else:
+            configName = options.config
+        commandArgs = [configName, targname, "{0}".format(redshift)]
         if "DL" in targTable.colnames:
             distance = targTable["DL"][loop]
             commandArgs.append(distance)
@@ -81,7 +94,7 @@ else: #If the target list is provided, fit the targets one by one.
         commandArgs.append(sedFile)
         if not options.refit: #Omit the target if there is a fitting result.
             fileList = os.listdir(".")
-            if "{0}.fltrs".format(targname) in fileList:
+            if "{0}.fitrs".format(targname) in fileList:
                 print("\n***{0} has been fitted!\n".format(targname))
                 continue
         commandDict["options"] = ["-o"] #We need to overwrite the target info.
@@ -89,5 +102,8 @@ else: #If the target list is provided, fit the targets one by one.
             commandDict["options"].append("-w")
         commandDict["args"] = commandArgs
         commandLine = makeCommand(commandDict)
-        print(commandLine)
-        os.system(commandLine)
+        try:
+            print(commandLine)
+            #os.system(commandLine)
+        except:
+            print("***Fail to run:\n    '{0}'!".format(commandLine))
